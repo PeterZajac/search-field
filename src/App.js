a749ba5093a5a07382263696ecbe38e88e8eede2
PeterZajac <zajacp011@gmail.com>
2024-02-25 14:03:17 +0100

    Add movie filtering functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import allMovies from "./data";
 import { useState, useEffect } from "react";
 function App() {
   const [searchingText, setSearchingText] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState([]);
   useEffect(() => {
-    console.log("jebau som ti mať");
+    const moviesAfterFilter = allMovies.filter((oneMovie) => {
+      const returnedMovie = oneMovie.title.toLowerCase();
+      return returnedMovie.includes(searchingText.toLowerCase());
+    });
+    setFilteredMovies(moviesAfterFilter);
   }, [searchingText]);
   return (
-    <div className="App">
+    <div className="movies-box">
       <form>
         <input
           type="input"
@@ -14,11 +19,11 @@ function App() {
           onChange={(e) => setSearchingText(e.target.value)}
         />
       </form>
-      <div>
-        {allMovies.map((oneMovie) => {
+      <div className="all-movies">
+        {filteredMovies.map((oneMovie) => {
           const { id, title, image, age, tags, description } = oneMovie;
           return (
-            <div key={id}>
+            <div key={id} className="one-movie">
               <img src={image} alt={description} />
               <h2>{title}</h2>
               <p>{age}</p>
